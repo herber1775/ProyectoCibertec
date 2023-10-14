470b2d8d93952cc0aecff8ec87967ecc0bc597b6
Herber Villegas <herber.villegas.hu@gmail.com>
2023-10-13 22:47:18 -0500

    crud Cliente Herber

--- src/main/java/pe/com/sisvapro/SistemaVentaAutosSisvapro/entity/Cliente.java
+++ src/main/java/pe/com/sisvapro/SistemaVentaAutosSisvapro/entity/Cliente.java
@@ -1,14 +1,8 @@
 package pe.com.sisvapro.SistemaVentaAutosSisvapro.entity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cliente")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,77 +13,77 @@ public class Cliente {
     private String dni;
 
     @Column(name = "nombre")
-    private String nom;
+    private String nombre;
 
     @Column(name = "apellido")
-    private String ape;
+    private String apellido;
 
     @Column(name = "direccion")
-    private String dir;
+    private String direccion;
 
     @Column(name = "correo")
     private String correo;
 
     @Column(name = "telefono")
-    private String tel;
-    
-    
-    public int getId_cli() {
-    	return idCli;
-    }
-    
-    public void setId_cli(int id_cli) {
-    	this.idCli = id_cli;
-    }
-    
-    public String getDni() {
-    	return dni;
-    }
-    
-    public void setDni(String dni) {
-    	this.dni = dni;
-    }
-    
-    public String getNom() {
-    	return nom;
-    }
-    
-    public void setNom(String nom) {
-    	this.nom = nom;
-    }
-    
-    public String getApe(){
-    	return ape;
-    }
-    
-    public void setDir(String dir) {
-    	this.dir = dir;
-    }
-    
-    public String getCorreo() {
-    	return correo;
-    }
-    
-    public void setCorreo(String correo) {
-    	this.correo = correo;
-    }
-    
-    public String getTelefono() {
-    	return tel;
-    }
-    
-    public void setTel(String tel) {
-    	this.tel = tel;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private String telefono;
+
+	public int getIdCli() {
+		return idCli;
+	}
+
+	public void setIdCli(int idCli) {
+		this.idCli = idCli;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	
     
     
     
