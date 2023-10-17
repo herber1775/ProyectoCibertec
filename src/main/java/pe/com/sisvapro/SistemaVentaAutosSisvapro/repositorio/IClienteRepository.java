6cf443e6e93d43eb62668b7559346fc38dde50ec
RuthGQ <rg02192004@gmail.com>
2023-10-17 15:46:37 -0500

    repository

--- src/main/java/pe/com/sisvapro/SistemaVentaAutosSisvapro/repositorio/IClienteRepository.java
+++ src/main/java/pe/com/sisvapro/SistemaVentaAutosSisvapro/repositorio/IClienteRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface IClienteRepository extends JpaRepository<Cliente, Integer> {
     public Cliente findByDni(String dni);
     
-    public Cliente findByNombre(String nom);
+    public Cliente findByNom(String nom);
     
-    public Cliente findByApellido(String ape);
+    public Cliente findByApe(String ape);
     
     
     
