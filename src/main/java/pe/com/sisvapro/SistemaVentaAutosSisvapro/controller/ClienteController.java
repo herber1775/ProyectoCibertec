1078adb2f11e612d8a8aadef9899d6c81e4ef709
HerberVillegasHuaman <herber.villegas.hu@gmail.com>
2023-11-15 22:49:42 -0500

    BackEnd-Cliente-Empleado

--- src/main/java/pe/com/sisvapro/SistemaVentaAutosSisvapro/controller/ClienteController.java
+++ src/main/java/pe/com/sisvapro/SistemaVentaAutosSisvapro/controller/ClienteController.java
@@ -1,4 +1,5 @@
 package pe.com.sisvapro.SistemaVentaAutosSisvapro.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
@@ -9,18 +10,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import pe.com.sisvapro.SistemaVentaAutosSisvapro.entity.Cliente;
+import pe.com.sisvapro.SistemaVentaAutosSisvapro.repositorio.IClienteRepository;
 import pe.com.sisvapro.SistemaVentaAutosSisvapro.servicio.ClienteService;
 
 @RestController
+@CrossOrigin("*")
+@AllArgsConstructor
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	
-	@Autowired
 	private ClienteService clienteservice;
+	IClienteRepository clienteRepository;
 	
 	@GetMapping("/cliente")
 	public List<Cliente> lisClient() {
@@ -43,5 +49,5 @@ public class ClienteController {
 	 public void deleteClient(@PathVariable int idcli){
 		 clienteservice.deleteClient(idcli);
 	    }
-		
+		//http://localhost:8080/
 }
--- src/main/java/pe/com/sisvapro/SistemaVentaAutosSisvapro/controller/EmpleadoController.java
+++ src/main/java/pe/com/sisvapro/SistemaVentaAutosSisvapro/controller/EmpleadoController.java
@@ -4,6 +4,7 @@ package pe.com.sisvapro.SistemaVentaAutosSisvapro.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,8 @@ import pe.com.sisvapro.SistemaVentaAutosSisvapro.servicio.EmpleadoService;
 @RestController
 @RequestMapping("/empleado")
 @AllArgsConstructor
+@CrossOrigin("*")
+
 public class EmpleadoController {
 	
     private EmpleadoService empleadoService;
@@ -35,20 +38,20 @@ public class EmpleadoController {
 	        return empleadoService.obtenerEmpleado(id);
 	    }
 	 
-	 @PostMapping("/insertar")
-		@ResponseBody
-		public Empleado agregarEmpleado(@RequestBody Empleado empleado) {
-			return empleadoService.agregarEmpleado(empleado);
-		}
+	 @PostMapping
+	 @ResponseBody
+	 public Empleado agregarEmpleado(@RequestBody Empleado empleado) {
+	   return empleadoService.agregarEmpleado(empleado);
+	 }
 	 
-	 @PutMapping("/actualizar")
+	 @PutMapping
 		@ResponseBody
 		public Empleado actualizarEmpleado(@RequestBody Empleado empleado) {
 			return empleadoService.actualizarEmpleado(empleado);
 		}
 	 
 
-	 @DeleteMapping("/eliminar/{id}")
+	 @DeleteMapping("/{id}")
 		public void eliminarEmpleado(@PathVariable int id) {
 		 empleadoService.eliminarEmpleado(id);
 		}
